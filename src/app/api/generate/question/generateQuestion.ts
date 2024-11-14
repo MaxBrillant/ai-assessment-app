da9322d7b6a17cc3ead55f6745f7daf46ed033b7
Max brillant <ndashimax37@gmail.com>
2024-11-14 16:17:00 +0300

    Changing the model and temperature to be more creative

--- src/app/api/generate/question/generateQuestion.ts
+++ src/app/api/generate/question/generateQuestion.ts
@@ -24,8 +24,8 @@ export const generateQuestion = async (
 
     const model = new ChatAnthropic({
       apiKey: process.env.ANTHROPIC_API_KEY,
-      model: "claude-3-5-sonnet-20241022",
-      temperature: 0.4,
+      model: "claude-3-5-haiku-20241022",
+      temperature: 0.5,
       maxTokens: 4096,
     });
 
