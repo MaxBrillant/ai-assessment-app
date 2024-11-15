8613f497d8a8603a5d86c7d3a776a594abfb8d29
Max brillant <ndashimax37@gmail.com>
2024-11-15 10:58:50 +0300

    Centering the loading message

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,9 @@ export default function Loading(props: { message?: string }) {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white/70 pointer-events-none z-[9999] backdrop-blur-sm">
       <RiLoader3Fill className="w-20 h-20 animate-spin opacity-30" />
-      {props.message && <p className="text-lg font-medium">{props.message}</p>}
+      {props.message && (
+        <p className="text-lg font-medium p-8 text-center">{props.message}</p>
+      )}
     </div>
   );
 }
