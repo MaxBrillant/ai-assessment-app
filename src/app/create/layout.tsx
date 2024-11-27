552ab17aa1a9301e2339b5d8f9b598b89b37201e
Max brillant <ndashimax37@gmail.com>
2024-11-27 15:29:08 +0300

    Attempting to fix the powerpoint issue

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -46,9 +46,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
