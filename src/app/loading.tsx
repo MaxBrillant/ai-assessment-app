bf4ac87a0508b807883c3453dacddf96cc5bc19c
Max brillant <ndashimax37@gmail.com>
2024-11-27 17:52:43 +0300

    New loading animation

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 import { RiLoader3Fill } from "react-icons/ri";
 export default function Loading(props: { message?: string }) {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white/70 pointer-events-none z-[9999] backdrop-blur-sm">
-      <RiLoader3Fill className="w-20 h-20 animate-spin opacity-30" />
+      <Image src="/loading.gif" width={400} height={100} alt="loading" />
       {props.message && (
         <p className="text-lg font-medium p-8 text-center">{props.message}</p>
       )}
